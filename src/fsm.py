f3c232b497347f4539a064566a7a29d38649a784
Sarah <serah.elrashidi@gmail.com>
2025-07-03 15:47:04 +0300

    Add FSMController

--- src/fsm.py
+++ src/fsm.py
@@ -1,17 +1,15 @@
 class FSMController:
-    """Finite-state wrapper for a single triple."""
-    def __init__(self, triple, detector_fn, healer_fn):
-        self.triple = triple
-        self.detect = detector_fn
-        self.heal   = healer_fn
-        self.state  = "q0"  # assume infected until checked
-
-    def step(self):
-        tags = self.detect(self.triple)
-        if tags:                       # infection(s) present
-            for t in tags:
-                self.triple = self.heal(self.triple, t) if self.triple else None
-            self.state = "q1" if self.triple else "qX"
-        else:
-            self.state = "q2"
-        return self.state, self.triple
+def **init**(self, triple, detector\_fn, healer\_fn):
+self.triple = triple
+self.detect = detector\_fn
+self.heal   = healer\_fn
+self.state  = "q0"
+def step(self):
+tags = self.detect(self.triple)
+if tags:
+for t in tags:
+self.triple = self.heal(self.triple, t) if self.triple else None
+self.state = "q1" if self.triple else "qX"
+else:
+self.state = "q2"
+return self.state, self.triple
